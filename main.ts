6c9f09994826ad324ae4477981f49252a0ee40c8
William Perron <hey@wperron.io>
2020-12-14 08:38:44 -0500

    add fizzbuzz function

--- main.ts
+++ main.ts
@@ -6,6 +6,20 @@ export function now() {
   return new Date();
 }
 
+export function* fizzbuzz(n: number) {
+  for (let i = 0; i < n; i++) {
+    if (i % 15 === 0) {
+      yield "FizzBuzz";
+    } else if (i % 5 === 0) {
+      yield "Buzz";
+    } else if (i % 3 === 0) {
+      yield "Fizz";
+    } else {
+      yield i;
+    }
+  }
+}
+
 export const add = (a, b) => a + b;
 export const sub = (a, b) => a - b;
 
